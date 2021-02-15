5381ec1036a3f2b57ddada8cefe3a52e16b667ed
오형욱 <nenonenoberneno@gmail.com>
2021-02-15 20:22:26 +0900

    Introduction to useState

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,50 @@
-import { useState } from 'react';
+import React, { Component, useState } from 'react';
 import './app.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [email, setEmail] = useState("");
-  const updateEmail = e => {
-    const {
-      target: {value}
-    } = e;
-    setEmail(value);
-  }
-
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => {setItem(item + 1);}
+  const decrementItem = () => {setItem(item - 1);}
   return (
     <>
-    {count}
-    <button onClick={() => setCount(count + 1)}>Increment</button>
-    <button onClick={() => setCount(count - 1)}>Decrement</button>
-    <input placeholder="Email" value={email} onChange={updateEmail} />
+    <div>{item}</div>
+    <button onClick={incrementItem}>Increment</button>
+    <button onClick={decrementItem}>Decrement</button>
     </>
   );
 }
 
+
+class UppUgly extends Component {
+  state = {
+    item: 1
+  }
+  render() {
+    const {item} = this.state;
+    return (
+      <div className={"App"}>
+        <h1>{item}</h1>
+        <button onClick={this.incrementItem}>Increment</button>
+        <button onClick={this.decrementItem}>Decrement</button>
+      </div>
+    );
+  }
+  incrementItem = () =>{
+    this.setState(state => {
+      return {
+        item: state.item + 1
+      }
+    })
+  }
+  decrementItem = () => {
+    this.setState(state => {
+      return {
+        item: state.item - 1
+      }
+    })
+  }
+}
+
 export default App;
+
+// export default UppUgly;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import UppUgly from './app';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <UppUgly /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
