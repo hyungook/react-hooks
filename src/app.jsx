38428252d05359b696ac2d6134fc2814ac1bbd3a
오형욱 <nenonenoberneno@gmail.com>
2021-02-15 23:52:51 +0900

    useInput part two

--- src/app.jsx
+++ src/app.jsx
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 import './app.css';
 
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) =>{
-    console.log(event.target);
+      const {target: {value}} = event;
+      
+    let willUpdate = true;
+    if(typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if(willUpdate) {
+      setValue(value);
+    }
   }
   return {value, onChange};
 }
 
 const App = () => {
-const name = useInput("Mr.")
+  // const maxLen = (value) => value.length <= 10;
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen)
+
   return (
     <>
     <h1>hello</h1>
