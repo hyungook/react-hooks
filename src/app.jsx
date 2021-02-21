833977ed20cbb0e975a7e15218e68b9ca760fb1f
오형욱 <nenonenoberneno@gmail.com>
2021-02-21 21:58:01 +0900

    useTitle

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,24 @@
 import React, { useEffect, useState } from "react";
 import './app.css';
 
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title")
+    htmlTitle.innerText = title;
+  }
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
 
 const App = () => {
 
-const [number, setNumber] = useState(0);
-const [aNumber, swtAnumber] = useState(0);
-
-const sayHello = () => console.log("hello")
-
-// useEffect(() => {
-//   sayHello();
-// })
-// useEffect(sayHello, [number]);
-useEffect(sayHello, []);
-
-// useEffect는 ComponentDidMount, ComponentWillMonut, ComponentDidUpdate 이다.
+  const titleUpdater = useTitle("Loading...")
+  setTimeout(() => titleUpdater("Home"), 5000);
 
   return (
     <div className="App">
       <h1>hello</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => swtAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
